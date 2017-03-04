9d961f203873141d2623873992f8f6cc1a1097aa
JesusRuiz <ruizluna.jesussalvador@gmail.com>
2017-03-03 22:46:19 -0700

    Initial Commit

---
+++ GoTravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoTravel
+//
+//  Created by Jesus Ruiz on 3/3/17.
+//  Copyright © 2017 AkibaTeaParty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
